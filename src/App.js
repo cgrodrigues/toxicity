6e146ab80ab4066753c25dd233f12841ff8b9ed9
cgrodrigues <carlos.garces.rodrigues@gmail.com>
2021-02-06 02:18:58 +0000

    Parser

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavbarText } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavbarText  } from 'reactstrap';
 import PreTrainedModel from './views/PreTrainedModel';
 import CreateTrainTest from './views/CreateTrainTest';
 
@@ -20,11 +20,11 @@ function App() {
             <Collapse isOpen={isOpen} navbar>
               <Nav className="mr-auto" navbar>
                 <NavItem>
-                  <NavLink exact to="/toxicity" activeStyle={{color: 'red'}}>Pre-Trained Model</NavLink>
+                  <NavLink exact to="/toxicity" activeStyle={{color: 'red'}} className="nav-link">Pre-Trained Model</NavLink>
                 </NavItem>
                 <NavItem>&nbsp;</NavItem>
                 <NavItem>
-                  <NavLink exact to="/toxicity/create_train_test" activeStyle={{color: 'red'}}>Create Train & Test</NavLink>
+                  <NavLink exact to="/toxicity/create_train_test" activeStyle={{color: 'red'}} className="nav-link">Create Train & Test</NavLink>
                 </NavItem>
               </Nav>
               <NavbarText>Toxic comment classification using Tensorflow and React.js</NavbarText>
